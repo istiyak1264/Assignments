f13c67bcb04860f735d81e3810b1c09cb88a8308
istiyak1264 <156337741+istiyak1264@users.noreply.github.com>
2024-12-02 09:50:34 +0600

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,18 @@
+
+public class ArithmeticOperators
+{
+    public static void main(String[] args)
+    {
+        int a = 100, b = 25;
+        System.out.print("Sum of a and b is: " + (a + b));
+        System.out.print("\n");
+
+        System.out.print("Sum of a and b is: " + (a - b));
+        System.out.print("\n");
+
+        System.out.print("Sum of a and b is: " + (a / b));
+        System.out.print("\n");
+
+        System.out.print("Sum of a and b is: " + (a % b));
+    }
+}
